115a21684c0b1aba2dff1ae126888b94407e307e
fernando.lopes <fernando.lopes@sankhya.com.br>
2020-01-15 15:43:04 -0300

    alteracao de script para validacao de campos e calculos de valores de campos regra baseada no tipo de evento definindo 7 cenariso de validacao

--- src/br/com/sankhya/mgs/ct/model/PrevisoesContratoModel.java
+++ src/br/com/sankhya/mgs/ct/model/PrevisoesContratoModel.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 public class PrevisoesContratoModel {
     private JapeWrapper dao;
     private DynamicVO vo;
+    private String regraVadalicao = "";
 
     private PrevisoesContratoModel() {
         inicialzaVariaveis();
@@ -36,6 +37,73 @@ public class PrevisoesContratoModel {
     public PrevisoesContratoModel validaDados() throws Exception {
         boolean postoPreechido = !(vo.asBigDecimalOrZero("CODTIPOPOSTO").equals(BigDecimal.ZERO));
         boolean servicoMaterialPreechido = !(vo.asBigDecimalOrZero("CODSERVMATERIAL").equals(BigDecimal.ZERO));
+        boolean quantidadePreenchido = !(vo.asBigDecimalOrZero("QTDCONTRATADA").equals(BigDecimal.ZERO) || vo.asBigDecimalOrZero("QTDCONTRATADA").equals(BigDecimal.ONE));
+        boolean valorPreechido = !(vo.asBigDecimalOrZero("VLRUNITARIO").equals(BigDecimal.ZERO));
+
+        String regraValidacao = getRegraValidacao();
+        if ("".equals(regraValidacao)) {
+            ErroUtils.disparaErro("Evento sem Tipo de Evento configurando não pode ser usado");
+        }
+        String erro = "";
+        switch (regraValidacao) {
+            case "P"://posto
+                if (!postoPreechido)
+                    erro += "Posto deve ser preenchido. ";
+                if (servicoMaterialPreechido)
+                    erro += "Serviço/Material não pode ser preechido. ";
+                if (!quantidadePreenchido)
+                    erro += "Quantidade deve ser preechida. ";
+                if (valorPreechido)
+                    erro += "Valor não pode ser preechido. ";
+                break;
+            case "C"://contrato
+                if (postoPreechido)
+                    erro += "Posto não deve ser preenchido. ";
+                if (servicoMaterialPreechido)
+                    erro += "Serviço/Material não pode ser preechido. ";
+                if (quantidadePreenchido)
+                    erro += "Quantidade não pode ser preechida. ";
+                if (!valorPreechido)
+                    erro += "Valor deve ser preechido. ";
+                break;
+            case "R"://rescisao
+                if (postoPreechido)
+                    erro += "Posto não deve ser preenchido. ";
+                if (servicoMaterialPreechido)
+                    erro += "Serviço/Material não pode ser preechido. ";
+                if (quantidadePreenchido)
+                    erro += "Quantidade não pode ser preechida. ";
+                if (valorPreechido)
+                    erro += "Valor não pode ser preechido. ";
+                break;
+            case "S1"://serviceo/material controle 1
+            case "S2"://serviceo/material controle 2
+                if (postoPreechido)
+                    erro += "Posto não deve ser preenchido. ";
+                if (!servicoMaterialPreechido)
+                    erro += "Serviço/Material deve ser preechido. ";
+                if (quantidadePreenchido)
+                    erro += "Quantidade não pode ser preechida. ";
+                if (!valorPreechido)
+                    erro += "Valor deve ser preechido. ";
+                break;
+            case "S3"://serviceo/material controle 3
+            case "S4"://serviceo/material controle 4
+                if (postoPreechido)
+                    erro += "Posto não deve ser preenchido. ";
+                if (!servicoMaterialPreechido)
+                    erro += "Serviço/Material deve ser preechido. ";
+                if (!quantidadePreenchido)
+                    erro += "Quantidade deve ser preechida. ";
+                if (valorPreechido)
+                    erro += "Valor nao pode ser preechido. ";
+                break;
+            default:
+                erro = "Regra não definida para Tipo de Evento: " + regraValidacao;
+        }
+        if (!"".equals(erro)) {
+            ErroUtils.disparaErro(erro);
+        }
 
         if (postoPreechido && servicoMaterialPreechido) {
             ErroUtils.disparaErro("Campos Tipos do Posto e Serviço/Material não podem ser preechidos no mesmo lançamento!");
@@ -43,71 +111,113 @@ public class PrevisoesContratoModel {
         return this;
     }
 
+    private String getRegraValidacao() throws Exception {
+        if (regraVadalicao.equals("")) {
+            DynamicVO eventoVO = JapeFactory.dao("TGFECUS").findByPK(vo.asBigDecimal("CODEVENTO"));
+            String tipoEvento = eventoVO.asString("TIPOEVENTO");
+
+            if ("S".equals(tipoEvento)) {
+                regraVadalicao = tipoEvento + vo.asBigDecimal("CODCONTROLE").toString();
+            }
+            regraVadalicao = tipoEvento;
+        }
+
+        return regraVadalicao;
+    }
+
     public void preecheCamposCalculados() throws Exception {
         boolean postoPreechido = !(vo.asBigDecimalOrZero("CODTIPOPOSTO").equals(BigDecimal.ZERO));
         boolean servicoMaterialPreechido = !(vo.asBigDecimalOrZero("CODSERVMATERIAL").equals(BigDecimal.ZERO));
+
         BigDecimal valorUnitario = BigDecimal.ZERO;
-        if (servicoMaterialPreechido) {
-            JapeWrapper mgsct_valores_produtosDAO = JapeFactory.dao("MGSCT_Valores_Produtos");
-            NativeSqlDecorator nativeSqlDDecorator = new NativeSqlDecorator(this, "sql/BuscaNumeroUnicoPrecoServicoMaterial.sql");
-            nativeSqlDDecorator.setParametro("NUMCONTRATO",vo.asBigDecimal("NUMCONTRATO"));
-            nativeSqlDDecorator.setParametro("NUMODALIDADE",vo.asBigDecimal("NUMODALIDADE"));
-            nativeSqlDDecorator.setParametro("CODSERVMATERIAL",vo.asBigDecimal("CODSERVMATERIAL"));
-            nativeSqlDDecorator.setParametro("CODEVENTO",vo.asBigDecimal("CODEVENTO"));
-            nativeSqlDDecorator.proximo();
-            BigDecimal numeroUnicoValoresProdutos = nativeSqlDDecorator.getValorBigDecimal("NUCONTRMATSRV");
-            if (numeroUnicoValoresProdutos == null){
-                ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
-            }
-
-            DynamicVO mgsct_valores_produtosVO = mgsct_valores_produtosDAO.findByPK(numeroUnicoValoresProdutos);
-            if (mgsct_valores_produtosVO == null){
-                ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
-            }
-
-            valorUnitario = mgsct_valores_produtosVO.asBigDecimal("VLRTOTAL");
-        } else if (postoPreechido){
-
-            JapeWrapper mgsct_valores_eventosDAO = JapeFactory.dao("MGSCT_Valores_Eventos");
-            NativeSqlDecorator nativeSqlDDecorator = new NativeSqlDecorator(this, "sql/BuscaNumeroUnicoPrecoPosto.sql");
-            nativeSqlDDecorator.setParametro("NUMCONTRATO",vo.asBigDecimal("NUMCONTRATO"));
-            nativeSqlDDecorator.setParametro("NUMODALIDADE",vo.asBigDecimal("NUMODALIDADE"));
-            nativeSqlDDecorator.setParametro("CODTIPOPOSTO",vo.asBigDecimal("CODTIPOPOSTO"));
-            nativeSqlDDecorator.setParametro("CODEVENTO",vo.asBigDecimal("CODEVENTO"));
-            nativeSqlDDecorator.proximo();
-            BigDecimal numeroUnicoValoresEventos = nativeSqlDDecorator.getValorBigDecimal("NUCONTREVENTO");
-            if (numeroUnicoValoresEventos == null){
-                ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
-            }
-
-            DynamicVO mgsct_valores_eventosVO = mgsct_valores_eventosDAO.findByPK(numeroUnicoValoresEventos);
-            if (mgsct_valores_eventosVO == null){
-                ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
-            }
-
-            valorUnitario = mgsct_valores_eventosVO.asBigDecimal("VLRTOTAL");
+        BigDecimal quantidade = BigDecimal.ZERO;
 
 
+        switch (getRegraValidacao()){
+            case "P"://posto
+                valorUnitario = getPrecoPosto();
+                if (BigDecimal.ZERO.equals(valorUnitario)) {
+                    ErroUtils.disparaErro("Preço de posto localizado não pode ser zero, favor verificar dados lancados!");
+                }
+                break;
+            case "C"://contrato
+            case "R"://rescisao
+            case "S1"://serviceo/material controle 1
+            case "S2"://serviceo/material controle 2
+                quantidade = BigDecimal.ONE;
+                break;
+            case "S3"://serviceo/material controle 3
+            case "S4"://serviceo/material controle 4
+                valorUnitario = getPrecoServicoMaterial();
+                if (BigDecimal.ZERO.equals(valorUnitario)) {
+                    ErroUtils.disparaErro("Preço de Material/Serviço localizado não pode ser zero, favor verificar dados lancados!");
+                }
+                break;
+            default:
 
         }
 
-        if (BigDecimal.ZERO.equals(valorUnitario)){
+
+        if (BigDecimal.ZERO.equals(valorUnitario)) {
             ErroUtils.disparaErro("Preço localizado não pode ser zero, favor verificar dados lancados!");
         }
 
         FluidUpdateVO fluidUpdateVO = dao.prepareToUpdate(vo);
-        fluidUpdateVO.set("VLRUNITARIO",valorUnitario);
-        fluidUpdateVO.set("VLRCONTRATADA",valorUnitario.multiply(vo.asBigDecimal("QTDCONTRATADA")));
+        fluidUpdateVO.set("VLRUNITARIO", valorUnitario);
+        fluidUpdateVO.set("VLRCONTRATADA", valorUnitario.multiply(vo.asBigDecimal("QTDCONTRATADA")));
         fluidUpdateVO.update();
+    }
 
+    private BigDecimal getPrecoPosto() throws Exception {
+        BigDecimal valorUnitario;
+        JapeWrapper mgsct_valores_eventosDAO = JapeFactory.dao("MGSCT_Valores_Eventos");
+        NativeSqlDecorator nativeSqlDDecorator = new NativeSqlDecorator(this, "sql/BuscaNumeroUnicoPrecoPosto.sql");
+        nativeSqlDDecorator.setParametro("NUMCONTRATO", vo.asBigDecimal("NUMCONTRATO"));
+        nativeSqlDDecorator.setParametro("NUMODALIDADE", vo.asBigDecimal("NUMODALIDADE"));
+        nativeSqlDDecorator.setParametro("CODTIPOPOSTO", vo.asBigDecimal("CODTIPOPOSTO"));
+        nativeSqlDDecorator.setParametro("CODEVENTO", vo.asBigDecimal("CODEVENTO"));
+        nativeSqlDDecorator.proximo();
+        BigDecimal numeroUnicoValoresEventos = nativeSqlDDecorator.getValorBigDecimal("NUCONTREVENTO");
+        if (numeroUnicoValoresEventos == null) {
+            ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
+        }
 
+        DynamicVO mgsct_valores_eventosVO = mgsct_valores_eventosDAO.findByPK(numeroUnicoValoresEventos);
+        if (mgsct_valores_eventosVO == null) {
+            ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
+        }
 
+        valorUnitario = mgsct_valores_eventosVO.asBigDecimal("VLRTOTAL");
+        return valorUnitario;
+    }
+
+    private BigDecimal getPrecoServicoMaterial() throws Exception {
+        BigDecimal valorUnitario;
+        JapeWrapper mgsct_valores_produtosDAO = JapeFactory.dao("MGSCT_Valores_Produtos");
+        NativeSqlDecorator nativeSqlDDecorator = new NativeSqlDecorator(this, "sql/BuscaNumeroUnicoPrecoServicoMaterial.sql");
+        nativeSqlDDecorator.setParametro("NUMCONTRATO", vo.asBigDecimal("NUMCONTRATO"));
+        nativeSqlDDecorator.setParametro("NUMODALIDADE", vo.asBigDecimal("NUMODALIDADE"));
+        nativeSqlDDecorator.setParametro("CODSERVMATERIAL", vo.asBigDecimal("CODSERVMATERIAL"));
+        nativeSqlDDecorator.setParametro("CODEVENTO", vo.asBigDecimal("CODEVENTO"));
+        nativeSqlDDecorator.proximo();
+        BigDecimal numeroUnicoValoresProdutos = nativeSqlDDecorator.getValorBigDecimal("NUCONTRMATSRV");
+        if (numeroUnicoValoresProdutos == null) {
+            ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
+        }
+
+        DynamicVO mgsct_valores_produtosVO = mgsct_valores_produtosDAO.findByPK(numeroUnicoValoresProdutos);
+        if (mgsct_valores_produtosVO == null) {
+            ErroUtils.disparaErro("Preço não localizado, favor verificar dados lancados!");
+        }
+
+        valorUnitario = mgsct_valores_produtosVO.asBigDecimal("VLRTOTAL");
+        return valorUnitario;
     }
 
     public void criaRegistrosDerivados() throws Exception {
         BigDecimal codigoTipoPosto = vo.asBigDecimalOrZero("CODTIPOPOSTO");
         boolean postoPreechido = !(codigoTipoPosto.equals(BigDecimal.ZERO));
-        if (postoPreechido){
+        if (postoPreechido) {
             DynamicVO tgfpssVO = JapeFactory.dao("TGFPSS").findByPK(codigoTipoPosto);
             String prefixoposto = tgfpssVO.asString("PREFIXOPOSTO");
             ArrayList<DynamicVO> vagaVOs = criaVagas(prefixoposto);
@@ -115,7 +225,7 @@ public class PrevisoesContratoModel {
         }
     }
 
-    private ArrayList<DynamicVO> criaVagas(String sigla ) throws Exception {
+    private ArrayList<DynamicVO> criaVagas(String sigla) throws Exception {
 
         BigDecimal quantidadeContratada = vo.asBigDecimal("QTDCONTRATADA");
 
@@ -126,7 +236,7 @@ public class PrevisoesContratoModel {
 
     private void criaPrevisaoVagas(ArrayList<DynamicVO> vagaVOs) throws Exception {
         VagasPrevisaoContratoModel vagasPrevisaoContratoModel = new VagasPrevisaoContratoModel();
-        for(DynamicVO vagaVO:vagaVOs) {
+        for (DynamicVO vagaVO : vagaVOs) {
             vagasPrevisaoContratoModel.criar(vo.asBigDecimal("NUCONTRPREV"), vagaVO.asBigDecimal("CODVAGA"));
         }
     }

76ce2c43acbd6d2b164da461366321b7c2810bf8
fernando.lopes <fernando.lopes@sankhya.com.br>
2020-01-27 17:10:24 -0300

    Script para criacoa de configuracao do modulo java na aplicacao

---
+++ src/br/com/sankhya/mgs/ct/model/PrevisoesUnidadeModel.java
@@ -0,0 +1,66 @@
+package br.com.sankhya.mgs.ct.model;
+
+import br.com.sankhya.bh.utils.ErroUtils;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.wrapper.JapeFactory;
+import br.com.sankhya.jape.wrapper.JapeWrapper;
+import java.math.BigDecimal;
+import java.util.Collection;
+
+/**
+ * Entidade: MGSCT_Previsoes_Unidade
+ * Tabela: MGSTCTUNIDADEPREV
+ * Chave: NUUNIDPREV
+ */
+public class PrevisoesUnidadeModel {
+    private JapeWrapper dao = JapeFactory.dao("MGSCT_Previsoes_Unidade");;
+    private DynamicVO vo;
+    public PrevisoesUnidadeModel()  {
+    }
+
+    public PrevisoesUnidadeModel(BigDecimal numeroUnico) throws Exception {//Chave: NUUNIDPREV
+        this.vo = dao.findByPK(numeroUnico);
+        inicialzaVariaveis();
+    }
+
+    public PrevisoesUnidadeModel(DynamicVO dynamicVO) throws Exception {
+        this.vo = dynamicVO;
+        inicialzaVariaveis();
+    }
+
+    public void setVo(DynamicVO vo) throws Exception {
+        this.vo = vo;
+        inicialzaVariaveis();
+    }
+
+    private void inicialzaVariaveis()throws Exception {
+
+    }
+
+    public void validaDadosInsert() throws Exception {
+        JapeWrapper PrevisoesContratoDAO = JapeFactory.dao("MGSCT_Previsoes_Contrato");
+        Collection<DynamicVO> dynamicVOS = PrevisoesContratoDAO.find("NVL(NUMCONTRATO,0) = ? AND NVL(CODTIPOPOSTO,0) = ? AND NVL(CODSERVMATERIAL,0) = ? AND NVL(CODEVENTO,0) = ?  AND NVL(CODCONTROLE,0) =?",
+                vo.asBigDecimalOrZero("NUMCONTRATO"),
+                vo.asBigDecimalOrZero("CODTIPOPOSTO"),
+                vo.asBigDecimalOrZero("CODSERVMATERIAL"),
+                vo.asBigDecimalOrZero("CODEVENTO"),
+                vo.asBigDecimalOrZero("CODCONTROLE")
+        );
+
+        if (dynamicVOS.size()==0){
+            ErroUtils.disparaErro("Não foi encontrado uma provisão do contrado com os mesmos dados da previsão unidade sendo lancada!");
+        }
+    }
+
+    private void validaDadosUpdate() throws Exception {
+
+    }
+
+    public void preecheCamposCalculados() throws Exception {
+
+    }
+
+    public void criaRegistrosDerivados() throws Exception {
+
+    }
+}

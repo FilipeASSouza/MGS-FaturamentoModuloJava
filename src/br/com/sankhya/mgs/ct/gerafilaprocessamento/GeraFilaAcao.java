a7558e6f05e81337b3a99407d026487e3bed8909
fernando.lopes <fernando.lopes@sankhya.com.br>
2020-03-02 11:01:19 -0300

    Correção: valida valores nulos para alterar de nuloa para zero como padrao de sem valor

--- src/br/com/sankhya/mgs/ct/gerafilaprocessamento/GeraFilaAcao.java
+++ src/br/com/sankhya/mgs/ct/gerafilaprocessamento/GeraFilaAcao.java
@@ -15,8 +15,18 @@ public class GeraFilaAcao implements AcaoRotinaJava {
             contextoAcao.setMensagemRetorno("Favor seleciona pelo menos um contrato");
         } else {
             Timestamp dataReferencia = Timestamp.valueOf(contextoAcao.getParam("DTREF").toString());
-            BigDecimal codigoUnidadeFaturamentoInicial = new BigDecimal(contextoAcao.getParam("CODSITEI").toString());
-            BigDecimal codigounidadeFaturamentoFinal = new BigDecimal(contextoAcao.getParam("CODSITEF").toString());
+            BigDecimal codigoUnidadeFaturamentoInicial = null;
+            if (contextoAcao.getParam("CODSITEI") != null) {
+                codigoUnidadeFaturamentoInicial = new BigDecimal(contextoAcao.getParam("CODSITEI").toString());
+            } else {
+                codigoUnidadeFaturamentoInicial = BigDecimal.ZERO;
+            }
+            BigDecimal codigounidadeFaturamentoFinal = null;
+            if (contextoAcao.getParam("CODSITEF") != null) {
+                codigounidadeFaturamentoFinal = new BigDecimal(contextoAcao.getParam("CODSITEF").toString());
+            } else {
+                codigounidadeFaturamentoFinal = BigDecimal.ZERO;
+            }
 
             for (Registro linha : linhas) {
 

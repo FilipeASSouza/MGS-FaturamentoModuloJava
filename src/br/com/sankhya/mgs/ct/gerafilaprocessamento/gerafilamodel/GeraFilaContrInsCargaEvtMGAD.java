8df03762c5b29579b9e43e74219cb4068441cc4d
fernando.lopes <fernando.lopes@sankhya.com.br>
2020-03-27 17:34:52 -0300

    Implementação: classes de detalhamento de custo para processamento das procedures CONTR_INS_CARGA_EVT_CCT, CONTR_INS_CARGA_EVT_DV, CONTR_INS_CARGA_EVT_DV_SUB, CONTR_INS_CARGA_EVT_M_GAD

---
+++ src/br/com/sankhya/mgs/ct/gerafilaprocessamento/gerafilamodel/GeraFilaContrInsCargaEvtMGAD.java
@@ -0,0 +1,42 @@
+package br.com.sankhya.mgs.ct.gerafilaprocessamento.gerafilamodel;
+
+import br.com.sankhya.mgs.ct.dao.FilaDAO;
+import com.sankhya.util.TimeUtils;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeraFilaContrInsCargaEvtMGAD extends GeraFilaSuper implements GeraFila {
+    public GeraFilaContrInsCargaEvtMGAD() {
+        super();
+    }
+
+    @Override
+    public boolean executar() throws Exception {
+        super.executar();
+
+        FilaDAO filaDAO = new FilaDAO();
+
+        BigDecimal defasagem = (BigDecimal) parametrosMetrica.get("DEFASAGEM");
+        Timestamp dataReferenciaCarga = new Timestamp(TimeUtils.add(getParametroTimestamp("dataReferencia").getTime(), defasagem.intValue(), Calendar.MONTH));
+
+        Map<String, String> mapParametrosChave = new HashMap<String, String>();
+
+        mapParametrosChave.put("V_CONTRATO", getParametroBigDecimal("numeroContrato").toString());
+        mapParametrosChave.put("V_TP_APONTAMENTO", "GDS");
+        mapParametrosChave.put("MES_CARGA", TimeUtils.getYearMonth(dataReferenciaCarga).toString());
+        mapParametrosChave.put("MES_FAT", TimeUtils.getYearMonth(getParametroTimestamp("dataReferencia")).toString());
+        mapParametrosChave.put("UP", getParametroBigDecimal("numeroUnidadeFaturamento").toString());
+        mapParametrosChave.put("P_PARAM", null);
+
+        String chave = geraChave(mapParametrosChave);
+
+        filaDAO.incializaFila(chave, getParametroString("nomeProcessamento"));
+        return true;
+    }
+
+
+}

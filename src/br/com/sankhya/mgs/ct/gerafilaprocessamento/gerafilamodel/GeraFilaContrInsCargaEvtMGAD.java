9aa8705c83ed40eba6a7dbf0edbee87b5260a598
fernando.lopes <fernando.lopes@sankhya.com.br>
2020-04-27 09:18:59 -0300

    correcao: Alteracao de parametro da procedure pegar das metricas

--- src/br/com/sankhya/mgs/ct/gerafilaprocessamento/gerafilamodel/GeraFilaContrInsCargaEvtMGAD.java
+++ src/br/com/sankhya/mgs/ct/gerafilaprocessamento/gerafilamodel/GeraFilaContrInsCargaEvtMGAD.java
@@ -21,6 +21,8 @@ public class GeraFilaContrInsCargaEvtMGAD extends GeraFilaSuper implements GeraF
         FilaDAO filaDAO = new FilaDAO();
 
         BigDecimal defasagem = (BigDecimal) parametrosMetrica.get("DEFASAGEM");
+        String parametroPeriodoApuracao = (String)parametrosMetrica.get("PERIODOAPURACAOGESTAO");
+
         Timestamp dataReferenciaCarga = new Timestamp(TimeUtils.add(getParametroTimestamp("dataReferencia").getTime(), defasagem.intValue(), Calendar.MONTH));
 
         Map<String, String> mapParametrosChave = new HashMap<String, String>();
@@ -30,7 +32,7 @@ public class GeraFilaContrInsCargaEvtMGAD extends GeraFilaSuper implements GeraF
         mapParametrosChave.put("MES_CARGA", TimeUtils.getYearMonth(dataReferenciaCarga).toString());
         mapParametrosChave.put("MES_FAT", TimeUtils.getYearMonth(getParametroTimestamp("dataReferencia")).toString());
         mapParametrosChave.put("UP", getParametroBigDecimal("numeroUnidadeFaturamento").toString());
-        mapParametrosChave.put("P_PARAM", null);
+        mapParametrosChave.put("P_PARAM", parametroPeriodoApuracao);
 
         String chave = geraChave(mapParametrosChave);
 

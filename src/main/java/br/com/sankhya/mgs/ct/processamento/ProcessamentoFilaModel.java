fc2f92e1a6fe70355110b4e5b02ad18acee3628c
Notebook Filipe ASSouza <filipe.sistemas@gmail.com>
2022-03-24 16:04:20 -0300

    atualizando - retirando o PerformanceMonitor do ProcessamentoFilaModel.

--- src/main/java/br/com/sankhya/mgs/ct/processamento/ProcessamentoFilaModel.java
+++ src/main/java/br/com/sankhya/mgs/ct/processamento/ProcessamentoFilaModel.java
@@ -91,7 +91,7 @@ public class ProcessamentoFilaModel {
         ProcessamentoFilaFactory processamentoFilaFactory = new ProcessamentoFilaFactory();
 
 
-        PerformanceMonitor.INSTANCE.measureJava("Carrega Fila " + nomeFila, () -> {
+        //PerformanceMonitor.INSTANCE.measureJava("Carrega Fila " + nomeFila, () -> {
             NativeSqlDecorator consultaFila = null;
             try {
                 System.out.println("Executando o run ProcessamentoFilaModel" + nomeFila);
@@ -103,7 +103,7 @@ public class ProcessamentoFilaModel {
                 consultaFila = new NativeSqlDecorator(this, nomeConsulta, this.jdbcWrapper);
                 consultaFila.setParametro("QTDEXECFILA", quantidadeExecucaoFila);
                 while (consultaFila.proximo()) {
-                    fila.add(new FilaPojo(consultaFila.getValorBigDecimal("NUFILAPROC"), consultaFila.getValorBigDecimal("NUTIPOPROC"), consultaFila.getValorString("CHAVE"), consultaFila.getValorString("NOME")));
+                    fila.add(new FilaPojo(consultaFila.getValorBigDecimal("NUFILAPROC"), consultaFila.getValorBigDecimal("NUTIPOPROC"), consultaFila.getValorString("CHAVE"), nomeFila));
                 }
             } catch (Exception e) {
                 throw new Exception("Erro ao executar consulta busca fila processamento: " + e);
@@ -111,10 +111,10 @@ public class ProcessamentoFilaModel {
                 if (consultaFila != null)
                     consultaFila.close();
             }
-        });
+        //});
         atualizarFilaeAguardarcommit();
         try {
-            PerformanceMonitor.INSTANCE.measureJava("adicionar Processamento " + nomeFila, () -> {
+            //PerformanceMonitor.INSTANCE.measureJava("adicionar Processamento " + nomeFila, () -> {
                 FilaUtils filaUtils = new FilaUtils(jdbcWrapper);
                 for (FilaPojo filaCod : fila) {
 
@@ -130,7 +130,7 @@ public class ProcessamentoFilaModel {
                     tp.execute(processamentoFilaParaleloModel);
 
                 }
-            });
+            //});
             if (tp.getQueue().isEmpty()) {
                 System.out.println("Finalizado");
             }
@@ -143,20 +143,21 @@ public class ProcessamentoFilaModel {
         FinalWrapper<Exception> exc = new FinalWrapper<>();
         Thread t = new Thread(() -> {
             try {
-                PerformanceMonitor.INSTANCE.measureJava("adicionar Processamento " + nomeFila, () -> {
+                //PerformanceMonitor.INSTANCE.measureJava("adicionar Processamento " + nomeFila, () -> {
                     JdbcWrapper jdbc = EntityFacadeFactory.getDWFFacade().getJdbcWrapper();
                     FilaUtils filaUtils = new FilaUtils(jdbc);
                     for (FilaPojo filaCod : fila) {
                         filaUtils.atualizarStatusFila(filaCod.getNUFILAPROC(), "A");
                     }
-                });
+                //});
                 if (tp.getQueue().isEmpty()) {
                     System.out.println("Finalizado");
                 }
             } catch (Exception e) {
                 exc.setWrapperReference(new Exception("Erro ao percorrer consulta busca fila processamento: " + e));
             }
-        });
+        //});
+});
         t.start();
         t.join();
         if (exc.getWrapperReference() != null)

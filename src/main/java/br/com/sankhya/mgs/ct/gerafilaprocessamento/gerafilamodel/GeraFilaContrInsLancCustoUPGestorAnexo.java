f051c0fb02d664b464b312ed24dc2f03a46c1d40
Juliano Lasmar <juliano@mgs.srv.br>
2022-05-30 17:29:06 -0300

    A planilha de gestor PDF não estava sendo gerada na fila. Percebemos que havia um NPE no momento de obter o param. numeroContrato, bem como que método setParametroExecucao estava sem implementação. A classe super estava sendo usada, mas essa não esperava os parâmetros da forma em que eles foram setados. Por isso, modificamos a classe filha para setar os parâmetros pois julgamos ser essa a alteração de menor impacto no restante das classes.
    #0 @0m

--- src/main/java/br/com/sankhya/mgs/ct/gerafilaprocessamento/gerafilamodel/GeraFilaContrInsLancCustoUPGestorAnexo.java
+++ src/main/java/br/com/sankhya/mgs/ct/gerafilaprocessamento/gerafilamodel/GeraFilaContrInsLancCustoUPGestorAnexo.java
@@ -3,28 +3,30 @@ package br.com.sankhya.mgs.ct.gerafilaprocessamento.gerafilamodel;
 import br.com.sankhya.mgs.ct.dao.FilaDAO;
 import com.sankhya.util.TimeUtils;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GeraFilaContrInsLancCustoUPGestorAnexo extends GeraFilaSuper implements GeraFila {
+    final Map<String, Object> parametros = new HashMap<>();
+
     public boolean executar() throws Exception {
         try{
 
             setParametroExecucao("nomeProcessamento","RtnContrInsLancCustoUPGestorAnexo");
 
-            Map<String, String> mapParametrosChave = new HashMap<String, String>();
-            mapParametrosChave.put("NUMCONTRATO", getParametroBigDecimal("numeroContrato").toString());
-            mapParametrosChave.put("CODUNIDADEFATUR", getParametroBigDecimal("numeroUnidadeFaturamento").toString());
-            mapParametrosChave.put("DTLANCCUSTO", TimeUtils.formataYYYYMMDD(getParametroTimestamp("dataCusto")));
-            mapParametrosChave.put("CODTIPOFATURA", getParametroBigDecimal("codigoTipoFatura").toString());
-
+            Map<String, String> mapParametrosChave = new HashMap<>();
+            mapParametrosChave.put("NUMCONTRATO", parametros.get("numeroContrato").toString());
+            mapParametrosChave.put("CODUNIDADEFATUR", parametros.get("numeroUnidadeFaturamento").toString());
+            mapParametrosChave.put("DTLANCCUSTO", TimeUtils.formataYYYYMMDD(parametros.get("dataCusto")));
+            mapParametrosChave.put("CODTIPOFATURA", parametros.get("codigoTipoFatura").toString());
 
             String chave = geraChave(mapParametrosChave);
 
             FilaDAO filaDAO = new FilaDAO();
             filaDAO.setComControleTransacao(true);
-            filaDAO.setCodigoUsuario(getParametroBigDecimal("codigoUsuario"));
-            filaDAO.incializaFila(chave, getParametroString("nomeProcessamento"));
+            filaDAO.setCodigoUsuario(new BigDecimal(parametros.get("codigoUsuario").toString()));
+            filaDAO.incializaFila(chave, parametros.get("nomeProcessamento").toString());
 
         }catch(Exception e){
             System.out.println("ERRO AO INICIA A FILA GeraFilaContrInsLancCustoUPGestorAnexo" + e);
@@ -40,6 +42,6 @@ public class GeraFilaContrInsLancCustoUPGestorAnexo extends GeraFilaSuper implem
 
     @Override
     public void setParametroExecucao(String nome, Object parametro) {
-
+        parametros.put(nome, parametro);
     }
 }
